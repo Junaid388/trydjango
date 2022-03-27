dc851e6401eac46df843ea8e9dfabf47bd3e7860
Khan <junaidkhan.5a4@gmail.com>
2022-03-27 14:10:10 +0530

    added app article

--- trydjango/views.py
+++ trydjango/views.py
@@ -4,12 +4,16 @@ To render HTML web pages
 
 from django.http import HttpResponse
 
-HTML_STRING ="""
-<h1>Hello World</h1>"""
 
 
-def home(request):
+
+def home_view(request):
     """
     Take in a request (Django sends request) and 
     return HTML as response (We pick to return the response"""
+    # from the database
+
+    #Django template
+    name = 'khan'
+    HTML_STRING =f"""<h1>Hello {name}!</h1>"""
     return HttpResponse(HTML_STRING)
