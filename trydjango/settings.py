4fd04a60db2958d6d48cf7ebf6673c95fb946e0e
Khan <junaidkhan.5a4@gmail.com>
2022-04-05 12:29:17 +0530

    environment variables and django-dotenv

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=l98rq0tohmk^h53h2bie%92nstu#+s394k=74gfwe6+d=_1t8'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-=l98rq0tohmk^h53h2bie%92nstu#+s394k=74gfwe6+d=_1t8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == '1'
 
 ALLOWED_HOSTS = []
-
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
